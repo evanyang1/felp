7b9d1aed0d1a38cafa0bdc84547ec67c7713c626
Kiara Wu <kiarapwu@gmail.com>
2020-03-03 11:15:34 -0800

    created html layout and grabbed API

---
+++ public/app.js
@@ -0,0 +1,9 @@
+
+const getPosts = () => {
+return fetch(`https://api.meaningcloud.com/sentiment-2.1?key=233c4b15af98df58daa1da749c297e2a&of=json&txt=Main%20dishes%20were%20quite%20good%2C%20but%20desserts%20were%20too%20sweet%20for%20me.&model=general&lang=en`)
+.then(res => res.json())
+.then(posts => console.log(posts))
+}
+
+
+getPosts()
