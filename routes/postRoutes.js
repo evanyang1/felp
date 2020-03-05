6c6ac281c6544463c03f743d63a800b49e8eb556
Evan Yang <evanyang@pop-os.localdomain>
2020-03-05 11:00:56 -0800

    folder organization

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const { Post } = require('../models')
 
-app.get('/posts', (req, res) => Post.findAll({ include: [User] })
+router.get('/posts', (req, res) => Post.findAll({ include: [User] })
   .then(posts => res.json(posts))
   .catch(e => console.error(e)))
 
-app.post('/posts', (req, res) => Post.create(req.body)
+router.post('/posts', (req, res) => Post.create(req.body)
   .then(() => res.sendStatus(200))
   .catch(e => console.error(e)))
+
+module.exports = router
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const { User } = require('../models')
 
-app.get('/users', (req, res) => User.findAll({ include: [Post] })
+router.get('/users', (req, res) => User.findAll({ include: [Post] })
   .then(users => res.json(users))
   .catch(e => console.error(e)))
 
-app.get('/users/:username', (req, res) => User.findOne({
+router.get('/users/:username', (req, res) => User.findOne({
     where: {
       username: req.params.username
     },
@@ -14,6 +14,8 @@ app.get('/users/:username', (req, res) => User.findOne({
     .then(user => res.json(user))
     .catch(e => console.error(e)))
 
-app.post('/users', (req, res) => User.create(req.body)
+router.post('/users', (req, res) => User.create(req.body)
     .then(() => res.sendStatus(200))
     .catch(e => console.error(e)))
+
+module.exports = router
