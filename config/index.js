0eefe1cb18cac2f77e75163732547439050b13b7
Evan Yang <evanyang@pop-os.localdomain>
2020-03-11 14:37:38 -0700

    delete everything

--- config/index.js
+++
@@ -1,4 +0,0 @@
-
-const Sequelize = require('sequelize')
-
-module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? process.env.JAWSDB_URL : process.env.LOCAL_URL)
--- models/index.js
+++
@@ -1,7 +0,0 @@
-const User = require('./User.js')
-const Post = require('./Post.js')
-
-User.hasMany(Post)
-Post.belongsTo(User)
-
-module.exports = { User, Post }
